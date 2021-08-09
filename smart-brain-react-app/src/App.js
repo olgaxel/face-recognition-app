a7c52714dfc8695b1e358572e6c9238823bfffaa
olgaxel <olga.brangeon@gmail.com>
2021-08-09 08:37:16 +0200

    fix signin end point

--- smart-brain-react-app/src/App.js
+++ smart-brain-react-app/src/App.js
@@ -72,7 +72,7 @@ loadUser=(data)=>{
   }
 
   onButtonSubmit =()=>{
-    fetch('hhttps://stark-badlands-27897.herokuapp.com/imageurl', {
+    fetch('https://stark-badlands-27897.herokuapp.com/imageurl', {
             method:'post',
             headers:{'Content-Type': 'application/json'},
             body: JSON.stringify({
